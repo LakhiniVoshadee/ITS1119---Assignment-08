2c840134eb36beb68b243acdddaaa049d0921ec0
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2024-11-07 14:59:47 +0530

    added js to case-02

--- js/case02.js
+++ js/case02.js
@@ -1,1 +1,12 @@
-let color = [red, green, blue, yellow, pink, purple];
+let color = ["red", "green", "blue", "yellow", "lightpink", "purple"];
+const boxes = document.querySelectorAll(".box");
+console.log(boxes);
+
+setInterval(() => {
+  let last = color.pop();
+  color.unshift(last);
+
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].style.backgroundColor = color[i];
+  }
+}, 2000);
